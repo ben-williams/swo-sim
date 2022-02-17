a31be92ab77c92e62df27269854afa5f4996b24f
Ben <ben.williams@noaa.gov>
2022-02-17 12:53:29 -0900

    add plot and table functions

--- R/analysis.R
+++ R/analysis.R
@@ -12,12 +12,6 @@ source("R/functions.R")
 # globals ----
 region = "GOA"
 
-# Set desired max sex sample sizes
-max_sx <- 150 # seq(50, 150, 25)
-
-# number of iterations
-iters <- 10
-
 # data ----
 
 vroom::vroom(here::here("data", "strata.csv")) %>%
@@ -41,56 +35,27 @@ vroom::vroom(here::here("data", "CPUE.csv")) %>%
   left_join(strata) %>%
   dplyr::select(year, species_code, catchjoin, hauljoin, stratum, numcpue, area) -> cpue
 
+sims(iters = 1, lfreq, cpue, samples = 20)
 
-
-# estimate expanded length comps with full dataset
-# also added the number of hauls and total number of lengths by haul
-og <- sims(iters = 1, lfreq, cpue)
-
+reps <- replicate(2, pop_est(lfreq, cpue, samples = 20), simplify = FALSE)
 
 # annual abundance by year & species
-ptm = proc.time()
-s20 <- sims(iters = 40, lfreq, cpue, samples = 20, strata = NULL)
-proc.time() - ptm
-# ~ 4 min
-s40 <- sims(iters = 40, lfreq, cpue, samples = 40, strata = NULL)
-s60 <- sims(iters = 40, lfreq, cpue, samples = 60, strata = NULL)
-s80 <- sims(iters = 40, lfreq, cpue, samples = 80, strata = NULL)
-s100 <- sims(iters = 40, lfreq, cpue, samples = 100, strata = NULL)
-s120 <- sims(iters = 40, lfreq, cpue, samples = 120, strata = NULL)
+og <- sims(iters = 1, lfreq, cpue, save = "og")
+s20 <- sims(iters = 2, lfreq, cpue, samples = 20, strata = NULL, save = "s2")
+s40 <- sims(iters = 20, lfreq, cpue, samples = 40, strata = NULL, save = "s40")
+s60 <- sims(iters = 20, lfreq, cpue, samples = 60, strata = NULL, save = "s60")
+s80 <- sims(iters = 20, lfreq, cpue, samples = 80, strata = NULL, save = "s80")
+s100 <- sims(iters = 20, lfreq, cpue, samples = 100, strata = NULL, save = "s100")
+s120 <- sims(iters = 20, lfreq, cpue, samples = 120, strata = NULL, save = "s120")
+s140 <- sims(iters = 20, lfreq, cpue, samples = 140, strata = NULL, save = "s140")
 
-getouts(og, save = "og.csv")
-getouts(s20, save = "s20.csv")
-getouts(s40, save = "s40.csv")
-getouts(s60, save = "s60.csv")
-getouts(s80, save = "s80.csv")
-getouts(s100, save = "s100.csv")
-getouts(s120, save = "s120.csv")
 
 # annual abundance by year & strata & species
-og_st <- sims(iters = 1, lfreq, cpue, samples = NULL, strata = TRUE)
-ptm = proc.time()
-s20_st <- sims(iters = 40, lfreq, cpue, samples = 20, strata = TRUE)
-proc.time() - ptm
-s40_st <- sims(iters = 40, lfreq, cpue, samples = 40, strata = TRUE)
-s60_st <- sims(iters = 40, lfreq, cpue, samples = 60, strata = TRUE)
-s80_st <- sims(iters = 40, lfreq, cpue, samples = 80, strata = TRUE)
-s100_st <- sims(iters = 40, lfreq, cpue, samples = 100, strata = TRUE)
-s120_st <- sims(iters = 40, lfreq, cpue, samples = 120, strata = TRUE)
-
-# save strata-based comps
-getouts(og_st, save = "og_st.csv")
-getouts(s20_st, samples = TRUE, save = "s20_comp.csv")
-getouts(s40_st, samples = TRUE, save = "s40_comp.csv")
-getouts(s60_st, samples = TRUE, save = "s60_comp.csv")
-getouts(s80_st, samples = TRUE, save = "s80_comp.csv")
-getouts(s100_st, samples = TRUE, save = "s100_comp.csv")
-getouts(s120_st, samples = TRUE, save = "s120_comp.csv")
-
-# save strata-
-getouts(s20_st, type = "removed", samples = TRUE, save = "s20_removed.csv")
-getouts(s40_st, type = "removed", samples = TRUE, save = "s40_removed.csv")
-getouts(s60_st, type = "removed", samples = TRUE, save = "s60_removed.csv")
-getouts(s80_st, type = "removed", samples = TRUE, save = "s80_removed.csv")
-getouts(s100_st, type = "removed", samples = TRUE, save = "s100_removed.csv")
-getouts(s120_st, type = "removed", samples = TRUE, save = "s120_removed.csv")
+og_st <- sims(iters = 1, lfreq, cpue, strata = TRUE, save = "og_st")
+s20_st <- sims(iters = 20, lfreq, cpue, samples = 20, strata = TRUE, save = "s20")
+s40_st <- sims(iters = 20, lfreq, cpue, samples = 40, strata = TRUE, save = "s40")
+s60_st <- sims(iters = 20, lfreq, cpue, samples = 60, strata = TRUE, save = "s60")
+s80_st <- sims(iters = 20, lfreq, cpue, samples = 80, strata = TRUE, save = "s80")
+s100_st <- sims(iters = 20, lfreq, cpue, samples = 100, strata = TRUE, save = "s100")
+s120_st <- sims(iters = 20, lfreq, cpue, samples = 120, strata = TRUE, save = "s120")
+s140_st <- sims(iters = 20, lfreq, cpue, samples = 140, strata = TRUE, save = "s140")
--- R/functions.R
+++ R/functions.R
@@ -2,6 +2,22 @@ library(tidyverse)
 library(vroom)
 library(here)
 library(purrr)
+ggplot2::theme_set(
+  ggplot2::theme_light() +
+    ggplot2::theme(
+      panel.grid.major = ggplot2::element_blank(),
+      panel.grid.minor = ggplot2::element_blank(),
+      # axis.ticks.length = grid::unit(base_ / 2.2, "pt"),
+      strip.background = ggplot2::element_rect(fill = NA, colour = NA),
+      strip.text.x = element_text(colour = "black"),
+      strip.text.y = element_text(colour = "black"),
+      panel.border = element_rect(fill = NA),
+      legend.key.size = grid::unit(0.9, "lines"),
+      legend.key = ggplot2::element_rect(colour = NA, fill = NA),
+      legend.background = ggplot2::element_rect(colour = NA, fill = NA)
+    )
+)
+
 
 pop_est <- function(lfreq, cpue, samples = NULL, yrs = 2017, strata = NULL){
 
@@ -119,11 +135,6 @@ pop_est <- function(lfreq, cpue, samples = NULL, yrs = 2017, strata = NULL){
       mutate(across(tidyr::everything(), ~replace_na(.x, 0))) %>%
       dplyr::select(year, species_code, stratum, length, males = `1`, females = `2`, unsexed = `3`) -> .out
 
-    if(!is.null(samples)){
-      list(new = .out, removed = .new_unsexed)
-      } else {
-        .out
-      }
   } else {
     .temp %>%
       dplyr::select(-stratum) %>%
@@ -132,26 +143,28 @@ pop_est <- function(lfreq, cpue, samples = NULL, yrs = 2017, strata = NULL){
       pivot_wider(names_from = sex, values_from = abund) %>%
       left_join(lngs, .) %>%
       mutate(across(tidyr::everything(), ~replace_na(.x, 0))) %>%
-      dplyr::select(year, species_code, length, males = `1`, females = `2`, unsexed = `3`)
+      dplyr::select(year, species_code, length, males = `1`, females = `2`, unsexed = `3`) -> .out
+  }
+
+  if(!is.null(samples)){
+    list(new = .out, removed = .new_unsexed)
+  } else {
+    .out
   }
 
 }
 
 
-sims <- function(iters = 1, lfreq, cpue, strata = NULL, samples = NULL,
-                 yrs = 2017,  save = NULL){
+sims <- function(iters = 1, lfreq, cpue, strata = NULL, samples = NULL, yrs = 2017,  save = NULL){
+
+  if(!is.null(samples) & is.null(save)){
+    stop("you have to save the 'newly unsexed' samples - aka save = 's20'" )
+  }
 
   .reps <- replicate(iters, pop_est(lfreq, cpue, samples, yrs, strata), simplify = FALSE)
 
-  if(is.null(samples) & is.null(save)){
-    .reps  %>%
-      purrr::map_df(., ~as.data.frame(.x), .id = "sim")
-  } else if(is.null(samples) & !is.null(save)){
-    .reps  %>%
-      purrr::map_df(., ~as.data.frame(.x), .id = "sim") -> .out
-    vroom::vroom_write(.out, here::here("output", save), delim = ",")
-    .out
-  } else if(!is.null(samples)){
+
+  if(!is.null(samples) & !is.null(save) ){
     do.call(mapply, c(list, .reps, SIMPLIFY = FALSE))$new %>%
       purrr::map_df(., ~as.data.frame(.x), .id = "sim") -> .new
     do.call(mapply, c(list, .reps, SIMPLIFY = FALSE))$removed %>%
@@ -160,11 +173,130 @@ sims <- function(iters = 1, lfreq, cpue, strata = NULL, samples = NULL,
     vroom::vroom_write(.new, here::here("output", paste0(save, "_comp.csv")), delim = ",")
     vroom::vroom_write(.removed, here::here("output", paste0(save, "_removed.csv")), delim = ",")
     .new
+  } else if(is.null(samples) & is.null(save)){
+    .reps  %>%
+      purrr::map_df(., ~as.data.frame(.x), .id = "sim")
+
   } else {
-    stop("huh?")
+    .reps  %>%
+      purrr::map_df(., ~as.data.frame(.x), .id = "sim") -> .out
+    vroom::vroom_write(.out, here::here("output", save), delim = ",")
+    .out
+
   }
+
 
 }
 
 
+get_data <- function(data, id, species = NULL, yrs = NULL){
 
+
+  data %>%
+    pivot_longer(cols = c(males, females, unsexed)) %>%
+    group_by(year, species_code, name, length) %>%
+    summarise(mean = mean(value),
+              se = sd(value) / sqrt(n()),
+              .group = "drop") %>%
+    mutate(lci = mean - se * 1.96,
+           uci = mean + se * 1.96,
+           id = id) -> .data
+
+  if(!is.null(species) & !is.null(yrs)){
+    .data %>%
+      dplyr::filter(species_code %in% species, year %in% yrs)
+
+  } else  if(!is.null(species) & is.null(yrs)){
+    .data %>%
+      dplyr::filter(species_code %in% species)
+
+  } else  if(is.null(species) & !is.null(yrs)){
+
+    .data %>%
+      dplyr::filter(year %in% yrs)
+
+  } else {
+    .data
+  }
+}
+
+
+plot_comp <- function(base_data, sim_data, species = NULL, yrs = NULL){
+
+  if("stratum" %in% names(base_data) | "stratum" %in% names(sim_data)){
+    stop("yeah, we aren't making all those plots \n use data that don't include strata...")
+  }
+  id1 = deparse(substitute(base_data))
+  id2 = deparse(substitute(sim_data))
+
+  get_data(base_data, id = id1, species, yrs) %>%
+    bind_rows(get_data(sim_data, id = id2, species, yrs)) -> .data
+
+
+  for (var in unique(.data$species_code)) {
+
+
+   print(ggplot(.data[.data$species_code==var,], aes(length, mean, color = id)) +
+             geom_line() +
+             geom_ribbon(aes(ymin = lci, ymax = uci), alpha = 0.2, linetype = 0) +
+             facet_wrap(name~year, scales = "free") +
+             scale_color_manual(values = c(4, 1)) +
+             scale_fill_manual(values = c(4, 1)) +
+             guides(color = guide_legend(override.aes = list(fill=NA))) +
+             ggtitle(paste0("species = ", var))
+   )
+
+   }
+}
+
+
+table_comp <- function(base_data, sim_data, species = NULL, yrs = NULL){
+
+  if("stratum" %in% names(base_data) & !("stratum" %in% names(sim_data)) |
+     !("stratum" %in% names(base_data)) & "stratum" %in% names(sim_data)){
+  stop("both files need to have stratum or not, can't be mixing them")
+  }
+
+  id1 = deparse(substitute(base_data))
+  id2 = deparse(substitute(sim_data))
+
+  get_data(base_data, id = id1, species, yrs) %>%
+    bind_rows(get_data(sim_data, id = id2, species, yrs)) -> .data
+
+  id1 = enquo(id1)
+  id2 = enquo(id2)
+
+  if("stratum" %in% names(base_data)){
+
+    .data %>%
+      group_by(year, species_code, stratum, name, id) %>%
+      summarise(mean = mean(mean),
+                .groups = "drop") %>%
+      pivot_wider(names_from = id, values_from = mean) %>%
+      mutate(diff = .data[[id1]] -.data[[id2]],
+             pd = diff / ((.data[[id1]] + .data[[id2]]) / 2) * 100)
+
+  } else {
+    .data %>%
+      group_by(year, species_code, name, id) %>%
+      summarise(mean = mean(mean),
+                .groups = "drop") %>%
+      pivot_wider(names_from = id, values_from = mean) %>%
+      mutate(diff = .data[[id1]] -.data[[id2]],
+             pd = diff / ((.data[[id1]] + .data[[id2]]) / 2) * 100)
+  }
+
+
+}
+
+
+plot_comp2 <- function(base_data, sim_data, species = NULL, yrs = NULL){
+
+
+  table_comp(base_data, sim_data, species, yrs) %>%
+    pivot_longer(cols = -c(year, species_code, name, diff, pd), names_to = "data") %>%
+    ggplot(aes(value/1000, group = data, fill = data)) +
+    geom_density(alpha = 0.3) +
+    facet_wrap(species_code~name, scales = "free", dir = "v")
+
+}

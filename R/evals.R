bd61c71c00f1388340059130c35465892c617a8a
Ben <ben.williams@noaa.gov>
2022-03-02 09:42:59 -0900

    stash evals

--- R/evals.R
+++ R/evals.R
@@ -24,6 +24,15 @@ s100_st <- vroom::vroom(here::here("output", "s100_comp.csv"))
 s120_st <- vroom::vroom(here::here("output", "s120_comp.csv"))
 s140_st <- vroom::vroom(here::here("output", "s140_comp.csv"))
 
+# iteration sizes
+s8020 <- vroom::vroom(here::here("output", "s8020_st_comp.csv"))
+s8040 <- vroom::vroom(here::here("output", "s8040_st_comp.csv"))
+s8060 <- vroom::vroom(here::here("output", "s8060_st_comp.csv"))
+s8080 <- vroom::vroom(here::here("output", "s8080_st_comp.csv"))
+s80100 <- vroom::vroom(here::here("output", "s80100_st_comp.csv"))
+s80120 <- vroom::vroom(here::here("output", "s80120_st_comp.csv"))
+
+
 # all years
 s80yr <- vroom::vroom(here::here("output", "s80yr.csv"))
 
@@ -48,3 +57,14 @@ plot_comp2(og, s120, species = c("10110", "10120"))
 plot_comp2(og, s140, species = c("10110", "10120"))
 
 plot_comp2(og, s80yr, species = c("10110", "10120"))
+
+t8020 <- table_comp(og_st, s8020, strata = TRUE)
+t8040 <- table_comp(og_st, s8040, strata = TRUE)
+t8060 <- table_comp(og_st, s8060, strata = TRUE)
+t8080 <- table_comp(og_st, s8080, strata = TRUE)
+t80100 <- table_comp(og_st, s80100, strata = TRUE)
+t80120 <- table_comp(og_st, s80120, strata = TRUE)
+
+t80100 %>%
+  ggplot(aes(diff, color = factor(stratum))) +
+  geom_density()
